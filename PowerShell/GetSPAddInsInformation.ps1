7a71fb7ef690aa3b8e4bf83980e014b7a63b8952
TRINQUIER Vannick <trinquier.vannick@live.com>
2016-02-13 21:34:07 +0700

    Retrieve SharePoint Add-Ins secret expiration date

--- PowerShell/GetSPAddInsInformation.ps1
+++ PowerShell/GetSPAddInsInformation.ps1
@@ -1,9 +1,23 @@
+# Get information about SharePoint Add-Ins already deployed
+# Among the information retrieve, we have the expiration date for SharePoint client secret
+# PS> .\GetSPAddInsInformation.ps1 -addIn <APPNAME>
+# Done by Vannick Trinquier 
+# On 13/02/2016
+
+
+[CmdletBinding()]
+Param(
+   [Parameter(Mandatory=$True)]
+   [string]$addIn
+)
+
 Connect-MsolService
-$applist = Get-MsolServicePrincipal -all  |Where-Object -FilterScript { ($_.DisplayName -like "*Kulikat*") }
+$applist = Get-MsolServicePrincipal -all  |Where-Object -FilterScript { ($_.DisplayName -like "*$addIn*") }
 foreach ($appentry in $applist)
 {
     $principalId = $appentry.AppPrincipalId
     $principalName = $appentry.DisplayName
+    Write-Host "----------------------------------`n"
     Write-Host "Name: $principalName"
 	Write-Host "Client Id: $principalId"
     
